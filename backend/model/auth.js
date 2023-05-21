e449041c89425f0fd074caf3c5a389b197532211
Nagesh kumar <96883726+nageshkumar95041@users.noreply.github.com>
2023-05-22 00:11:08 +0530

    Update auth.js
    process.env add kiye

--- backend/model/auth.js
+++ backend/model/auth.js
@@ -1,6 +1,8 @@
 const jwt=require("jsonwebtoken");
 const userCollection=require("./userSchema")
 const cookieParser=require("cookie-parser")
+const dotenv=require("dotenv").config();
+const secretKey=process.env.SECRETKEY;
 
 const auth=async(req,res,next)=>{
     try{
@@ -8,7 +10,7 @@ const auth=async(req,res,next)=>{
 
         if(token){
 
-            const verifyUser=jwt.verify(token,"dlfdldiojdfdfjdoffueyrieihkdfdflhfofhfdlfldffdrerbghykii")
+            const verifyUser=jwt.verify(token,secretKey)
             req.auth=true
         }
         else{
